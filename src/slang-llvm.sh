069743c9e7ca97ecac78e486ba80b1d8c47a60cf
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2019-05-05 20:06:31 +0300

    Compilation script created

---
+++ src/slang-llvm.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+usage() { echo "Usage: ${0} [-o <target_name>] <source_name>" 1>&2; exit 1; }
+
+while getopts ":ho:" arg; do
+    case "${arg}" in
+        o)
+            out_path=${OPTARG}
+            ;;
+        h | *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${1}" ]; then
+    usage
+fi
+
+in_path="${1}"
+
+if [ -z "${out_path}" ]; then
+    out_path="${in_path}.out"
+fi
+
+install_path=INSTALL_PATH_BY_CMAKE
+
+"$install_path/slang-compiler/parser/SLang Compiler" /json "${in_file}"
+"$install_path/slang-compiler/slang_jtll" "${in_path}.json"
+rm "${in_path}.json"
+clang -o "${out_path}" app.o
+rm app.o
