8dedbb68206a341419fb7980338ac9a97785d115
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2021-11-10 10:20:30 +0300

    Textual refactoring of documentation and CMake configs (#13)
    * Add Markdown formatting settings
    
    * Reformat CMake files
    
    * Refactor documenting comments

--- app/code_generator.cc
+++ app/code_generator.cc
@@ -4,17 +4,16 @@
 
 /**
  * \brief Convert c-style arguments of \c main into modern C++ structures.
- * \param argv Pointer to array of c-style strings of arguments (like <tt>main</tt>'s \c argv).
- * \param argc Size of the array \c argv (like <tt>main</tt>'s \c argc).
+ * \param[in] argv Pointer to array of c-style strings of arguments (like <tt>main</tt>'s \c argv).
+ * \param[in] argc Size of the array \c argv (like <tt>main</tt>'s \c argc).
  * \return \c std::vector of \c std::string_view pointing to values pointed by elements of \p argv.
  * \pre \p argv is pointer to contiguous memory block containing \p argc number of elements.
  * \pre Each element pointed by elements of \p argv is null-terminated c-style string.
  * \note The order of parameters was changed due to possible usage of \c std::span.
+ * \todo use `const std::span<const char *> &` instead of `argv` and `argc` in C++20.
  */
-static auto ArgsToVector(const char *const *const argv, const int argc) noexcept
-// TODO: use `const std::span<const char *> &` instead of `argv` and `argc` in
-// C++20.
-{
+static auto ArgsToVector(const char *const *const argv,
+                         const int argc) noexcept {
   std::vector<std::string_view> result;
   result.reserve(argc);
 
--- src/ir_converter/ir_converter.h
+++ src/ir_converter/ir_converter.h
@@ -1,3 +1,9 @@
+/**
+ * \file
+ * SLang Intermediate Representation and related operations.
+ * \author Denis Chernikov (@deiuch)
+ * \date 2021
+ */
 #ifndef SLANG_CODEGEN_LLVM_IR_CONVERTER_HPP
 #define SLANG_CODEGEN_LLVM_IR_CONVERTER_HPP
 
@@ -15,7 +21,7 @@ using LlvmIr = llvm::Module;
 
 /**
  * \brief Convert program represented by SLang IR into LLVM IR Module.
- * \param program SLang IR object with program to convert.
+ * \param[in] program SLang IR object with program to convert.
  * \return \c nullptr - conversion failed; LLVM IR Module with program - otherwise.
  */
 [[nodiscard]] std::unique_ptr<const LlvmIr> Convert(
