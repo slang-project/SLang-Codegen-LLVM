1331079586af81db221a39359a465cd338a44a4d
Denis Chernikov <tchernikoff.denis2013@yandex.ru>
2021-11-21 14:34:36 +0300

    Add header file template

---
+++ .vscode/templates/header.h
@@ -0,0 +1,13 @@
+/**
+ * \file
+ * \author ${author}
+ * \date ${dateTimeFormat:year-only:}
+ */
+#ifndef SLANG_CODEGEN_LLVM_${fileBasename}
+#define SLANG_CODEGEN_LLVM_${fileBasename}
+
+namespace slang::llvm_code_generator {
+  // TODO
+} // namespace slang::llvm_code_generator
+
+#endif
